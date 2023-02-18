4d431b000a6d54b76fff61d422dfe175ddb6993e
Chasing1020 <chasing1020@gmail.com>
2023-02-18 11:43:03 +0800

    Update AXI4MemoryTest

--- src/main/scala/memory/Memory.scala
+++ src/main/scala/memory/Memory.scala
@@ -49,13 +49,14 @@ class AXI4Memory(filePath: String = "") extends AXI4Module {
   // todo: add DecoupledIO fire
   // fixme: wrong AXI4Memory.ar result
   val read = mem.read(io.ar.bits.addr, true.B).asUInt
-  io.r.bits.data := EndianReverser(BeatBytes * 8, read).asUInt
+  io.r.bits.data := EndianReverser(BeatBytes * 8, read)
+//  io.r.bits.data := read
   io.r.bits.resp := RespOkay
   io.r.valid := true.B
 
-  Debug(cf"[AXI4Memory.aw]: ${io.aw.bits.addr}%x")
-  Debug(cf"[AXI4Memory.w ]: ${io.w.bits.data}%x")
-  Debug(cf"[AXI4Memory.b ]: ${io.b.bits.resp}%x")
+//  Debug(cf"[AXI4Memory.aw]: ${io.aw.bits.addr}%x")
+//  Debug(cf"[AXI4Memory.w ]: ${io.w.bits.data}%x")
+//  Debug(cf"[AXI4Memory.b ]: ${io.b.bits.resp}%x")
   Debug(cf"[AXI4Memory.ar]: ${io.ar.bits.addr}%x")
   Debug(cf"[AXI4Memory.r ]: ${io.r.bits.data}%x")
 }
--- src/test/scala/memory/AXI4MemoryTest.scala
+++ src/test/scala/memory/AXI4MemoryTest.scala
@@ -14,12 +14,16 @@ class AXI4MemoryTest extends AnyFlatSpec with ChiselScalatestTester {
     test(new AXI4Memory("./tests/hex/addi.hex")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
       dut.io.ar.bits.addr.poke(0.U)
       dut.clock.step(1)
-//      dut.io.out.inst.expect("0x1050003".U)
-      dut.io.ar.bits.addr.poke(4.U)
-      dut.clock.step(1)
+      dut.io.r.bits.data.expect("x06428293".U)
 
-      dut.io.ar.bits.addr.poke(8.U)
+      //      dut.io.out.inst.expect("0x1050003".U)
+      dut.io.ar.bits.addr.poke(1.U)
       dut.clock.step(1)
+      dut.io.r.bits.data.expect("x06430313".U)
+
+      dut.io.ar.bits.addr.poke(2.U)
+      dut.clock.step(1)
+      dut.io.r.bits.data.expect("x06438393".U)
     }
 
     (new ChiselStage).emitVerilog(
